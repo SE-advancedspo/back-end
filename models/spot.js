688d2e336fd2bd724e4e13aff57b0d64f93bfeac
Davide <36045489+Dovday@users.noreply.github.com>
2022-12-03 22:29:59 +0100

    Added SpotSchema and EventoSchema

---
+++ models/spot.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); //import mongoose
+
+// spot schema
+const SpotSchema = new mongoose.Schema({
+    id_spot: {type: Number, required: true}, // DC: necessary?
+    testo: {type: String, required: true},
+    autore: {type: String, required: true}, // DC: type: user?
+    num_voti: {type: Number, min: 0}, // DC: init to 0
+    altezza: Number, 
+    regione: String,
+    colore_capelli: String,
+    barba: Boolean,
+    facolta: String,
+    lang: String, // DC: it should be enum
+});
+
+
+const Spot = mongoose.model('spot', SpotSchema); //convert to model named spot
+module.exports = Spot; //export for controller use
