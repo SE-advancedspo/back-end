a5833eaf1106fa5a0b4518e35256970c32b14d0e
Davide Carlomagno <dvdcarlomagno@gmail.com>
2022-12-09 14:27:52 +0100

    Added comment for luogo attribute

--- models/evento.js
+++ models/evento.js
@@ -5,7 +5,7 @@ const EventoSchema = new mongoose.Schema({
     id_evento: {type: Number, required: true}, // DC: necessary?
     nome: {type: String, required: true},
     data: {type: Date, required: true},
-    luogo: {type: String, required: true},
+    luogo: {type: String, required: true}, // must be complete (via + civico + città)
     salvato: Boolean,
 });
 
