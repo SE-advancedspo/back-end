554cdf97e83ccb599026bfdf9dec01d467f802c5
Vincenzo Netti <vincenzontt02@gmail.com>
2022-12-12 18:40:26 +0100

    Testing

--- testing/api.test.js
+++ testing/api.test.js
@@ -1,6 +1,10 @@
 const fetch = require("node-fetch");
+const supertest = require("supertest"); 
+const app = require('../app');
+const request = require('supertest');
 
 let url = "http://localhost:8080"
+let url2 = "http://localhost:8080"
 
 describe('test-coverage degli user', () => {
     url += "/user"
@@ -9,18 +13,45 @@ describe('test-coverage degli user', () => {
         expect.assertions(1)
         expect((await fetch(url)).status).toEqual(200)
     })
-    test('Inserimento di un utente correttamente', async () => {
-        expect.assertions(1)
-        var response = await fetch(url,{
-                method: 'POST', body: JSON.stringify({
-                    "username": "Francesco1",
-                    "email": "francescoVerdolini@gmail.com",
-                    "password": "verde32",
-                    "contatto": "telegram.com"    
-                }),
-            headers: { 'Content-Type': 'application/json' }
-            })
-        expect((await response).status).toEqual(200)
+    test('Registrazione di un utente correttamente', async () => {
+        var payload={
+            "username": "Beenc2xa14",
+            "email": "franceslini@gmail.com",
+            "password": "verde32",
+            "contatto": "telegram.com"    
+        }
+        const res=await request(url2).post('/user')
+        .set('Content-type', 'application/json')
+        .send( payload )
+        .expect(200);
+        expect(res.body.message).toEqual("Utente registrato con successo");
     })
+    test('Inserimento di un utente già registrato', async () => {
+        var payload={
+            "username": "Beenca14",
+            "email": "franceslini@gmail.com",
+            "password": "verde32",
+            "contatto": "telegram.com"    
+        }
+        const res=await request(url2).post('/user')
+        .set('Content-type', 'application/json')
+        .send( payload )
+        .expect(200);
+        expect(res.body.message).toEqual("User already exists");
+    })
+    test('Inserimento di un utente senza specifico username', async () => {
+        var payload={
+            "email": "franceslini@gmail.com",
+            "password": "verde32",
+            "contatto": "telegram.com"    
+        }
+        const res=await request(url2).post('/user')
+        .set('Content-type', 'application/json')
+        .send( payload )
+        .expect(200);
+        expect(res.body.Error.errors.username.name).toEqual("ValidatorError");
+    })
+
+
 
 });
