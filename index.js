261bb04834d1c806731e2c2657a8871a0d4bb7c6
Davide Carlomagno <dvdcarlomagno@gmail.com>
2022-12-19 19:22:25 +0100

    Other commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey our API is running 🥳')
+})
+
+// Export the Express API
+module.exports = app
