6af2962f44ce262e6f9beec4228d5cce719d3ebb
Davide Carlomagno <dvdcarlomagno@gmail.com>
2022-12-24 11:11:32 +0100

    Added /api/index.js for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv').config()
+const express=require('express')
+const app=express()
+const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express')
+var bodyParser = require('body-parser');
+swaggerDocument = require('./swagger.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+	console.log("Listening on port " + listener.address().port)
+})
+
+const routes = require('./routes/routes')
+app.use('/', routes)
+
+mongoose.connect(
+	process.env.MONGODB_URI,
+	{useNewUrlParser: true, useUnifiedTopology: true},
+	(err) => {
+		if (err) return console.log("Error: ", err)
+		console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+	}
+);
+
